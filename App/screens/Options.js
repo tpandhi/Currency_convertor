f05a1f0990388575841e0441a7fa32b43da85362
Tanya Pandhi <pandhitanya@gmail.com>
2020-12-28 17:39:15 +0530

    intial commit

---
+++ App/screens/Options.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Linking, Alert } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+import colors from '../constants/colors';
+import { RowItem, RowSeparator } from '../components/RowItem';
+
+const openLink = (url) =>
+  Linking.openURL(url).catch(() =>
+    Alert.alert('Sorry, something went wrong.', 'Please try again later.')
+  );
+
+export default () => {
+  return (
+    <SafeAreaView style={{marginTop:"20px"}}>
+      <ScrollView >
+        <RowItem
+          title="Themes"
+          onPress={() => alert('todo!')}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
+
+        <RowSeparator />
+
+        <RowItem
+          title="React Native Basics"
+          onPress={() => openLink(
+            'https://learn.reactnativeschool.com/p/react-native-basics-build-a-currency-converter'
+          )
+        }
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+
+        <RowSeparator />
+
+        <RowItem
+          title="React Native by Example"
+          onPress={() => openLink('https://reactnativebyexample.com')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
